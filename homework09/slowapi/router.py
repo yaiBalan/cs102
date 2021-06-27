ce7bec70bda5420e6ab794b96ad8248e3bc4112b
yaiBalan <imexikashka@gmail.com>
2021-06-27 22:25:55 +0300

    Homework09

--- homework09/slowapi/router.py
+++ homework09/slowapi/router.py
@@ -2,10 +2,11 @@ import dataclasses
 import re
 import typing as tp
 
+from slowapi.response import Response
+
 
 @dataclasses.dataclass
 class Route:
     path: str
     method: str
-    func: tp.Callable
-
+    func: tp.Callable[..., Response]
